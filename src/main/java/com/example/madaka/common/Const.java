28e1a920ec4bc77167790acda17bf33d5017167f
ta-ebihara <ta-ebihara@ac-tec.co.jp>
2025-10-01 18:53:48 +0900

    ログイン画面実装途中

--- src/main/java/com/example/madaka/common/Const.java
+++ src/main/java/com/example/madaka/common/Const.java
@@ -1,5 +1,8 @@
 package com.example.madaka.common;
 
 public class Const {
-
+	/** ログイン画面名 */
+	 public static String screenName = "ログイン画面";
+	/** システム名 */
+	 public static String AppName = "勤怠連絡システム";
 }
--- src/main/java/com/example/madaka/service/LoginService.java
+++ src/main/java/com/example/madaka/service/LoginService.java
@@ -1,5 +1,23 @@
 package com.example.madaka.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.stereotype.Service;
+
+import com.example.madaka.form.LoginForm;
+import com.example.madaka.mapper.LoginMapper;
+
+@Service
 public class LoginService {
 
+	 private static LoginMapper loginMapper;
+
+	    @Autowired
+	    public LoginService(LoginMapper loginMapper) {
+	        this.loginMapper = loginMapper;
+	    }
+
+	    public static User login(LoginForm form) {
+	        return loginMapper.login(form);
+	    }
 }
