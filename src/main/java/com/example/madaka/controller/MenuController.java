bd95552a0ab67f66deb05d8a7ae4c239fdc1765d
ri-hasebe <ri-hasebe@DESKTOP-LIVJ5TC>
2026-02-17 18:59:47 +0900

    ハンバーガーメニューの取り込み

--- src/main/java/com/example/madaka/controller/MenuController.java
+++ src/main/java/com/example/madaka/controller/MenuController.java
@@ -1,12 +1,14 @@
 package com.example.madaka.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/madaka")
 public class MenuController {
-	@RequestMapping("/menu")
-	public String main(){
-		return "menu";
-	}
+    @GetMapping("/menu")
+    public String main(){
+        return "menu";
+    }
 }
